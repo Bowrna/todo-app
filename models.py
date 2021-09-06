ef135e8d29ea14b6ec0ac061c32266fccaaa6c0a
Bowrna <mailbowrna@gmail.com>
2021-09-06 16:00:49 +0530

    fix for listing the todo

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import sqlite3
+from collections import namedtuple
 
 class Schema:
     def __init__(self):
@@ -8,23 +9,25 @@ class Schema:
         self.create_tags_table()
 
     def create_tags_table(self):
+        print("Creating tags table")
         query = """
         CREATE TABLE IF NOT EXISTS "Tags" (
           id INTEGER PRIMARY KEY,
-          Tag TEXT
+          Tag TEXT,
           UserId INTEGER FOREIGNKEY REFERENCES User(_id)
         );
         """
         self.conn.execute(query)
 
     def create_todo_table(self):
+        print("Creating todo table")
         query = """
         CREATE TABLE IF NOT EXISTS "Todo" (
           id INTEGER PRIMARY KEY,
           Title TEXT,
           Description TEXT,
-          _is_done boolean DEFAULT 0,
-          _is_deleted boolean DEFAULT 0,
+          _is_done INTEGER NOT NULL DEFAULT 0 CHECK(_is_done in (0,1)),
+          _is_deleted INTEGER NOT NULL DEFAULT 0 CHECK(_is_deleted in (0,1)),
           CreatedOn Date DEFAULT CURRENT_DATE,
           DueDate Date,
           Priority INTEGER,
@@ -35,6 +38,7 @@ class Schema:
         self.conn.execute(query)
 
     def create_user_table(self):
+        print("Creating user table")
         query = """
         CREATE TABLE IF NOT EXISTS "User" (
           id INTEGER PRIMARY KEY,
@@ -49,30 +53,32 @@ class ToDoModel:
 
     def __init__(self):
         self.conn = sqlite3.connect('todo.db')
+        self.cursor = self.conn.cursor()
 
     def create(self, text, description):
-        query = f'insert into {self.TABLENAME} ' \
+        print(text)
+        print(description)
+        query = f'insert into {self.TABLENAME}' \
                 f'(Title, Description) ' \
                 f'values ("{text}","{description}")'
-        
-        result = self.conn.execute(query)
-        print(result.lastrowid)
-        # return result 
-        # uncomment above line and check what error occurs and why
+        print(query)
+        result = self.cursor.execute(query)
+        self.conn.commit()
+        self.cursor.close()
         return result.lastrowid
 
     def list_items(self, where_clause=""):
-        test_query = f"SELECT * " \
-                f"from {self.TABLENAME} "
-        print(test_query)
-        test_result_set = self.conn.execute(test_query).fetchall()
-        print(test_result_set)
+        # test_query = f"SELECT * " \
+        #         f"from {self.TABLENAME} "
+        # print(test_query)
+        # test_result_set = self.cursor.execute(test_query).fetchall()
+        # print(test_result_set)
+        fields = ['id', 'Title', 'Description', 'Is_done']
+        todo = namedtuple('todo', fields)
         query = f"SELECT id, Title, Description, _is_done " \
                 f"from {self.TABLENAME} WHERE _is_deleted != {1} " + where_clause
-        print (query)
         result_set = self.conn.execute(query).fetchall()
-        print(result_set)
-        result = [{column: row[i]
-                  for i, column in enumerate(result_set[0].keys())}
-                  for row in result_set]
+        result = [todo(*column)._asdict()
+                  for i, column in enumerate(result_set)]
         return result
+
